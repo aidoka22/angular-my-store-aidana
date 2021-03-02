c5f2d275005d90eac6c921f349a799b4f827a718
aidoka22 <aidana.sharipbay@gmail.com>
2021-03-03 01:15:40 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-my-store-aidana
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-my-store-aidana)
---
+++ src/app/products.ts
@@ -0,0 +1,245 @@
+export const products = [
+  { category_id: 0,
+    category_name: "Makeup",
+    display_flag: false, 
+    items: [{
+    id: 1,
+    name: "Lash Princess False Lash Effect Mascara ",
+    price: 4.99,
+    rating:2,
+    description: "If you receive this mascara UNWRAPPED, it did not come from essence cosmetics.NO FALSIES NEEDED! Lash Princess False Lash Mascara defines and separates lashes while achieving a bold look.DRAMATIC VOLUME: the conic shape fiber brush delivers dramatic volume and sculpted length WITHOUT clumps or globs.LASTS ALL DAY: go about your day free from flaking & fading! This mascara does not flake, fade, or wear, making it a perfect all-day, longlasting mascara.CRUELTY FREE: essence cosmetics is certified and acknowledged by PETA as a cruelty-free brand. We do not test any of our products on animals.",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/61nvmVTF12L._SL1500_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/71vNRRHMLvL._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/31cd26kbBLL.jpg",
+    likes: 250,link:"https://www.amazon.com/essence-Princess-Effect-Mascara-Cruelty/dp/B00T0C9XRK/ref=sr_1_1?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=CN80136S063SQVAKX3Q8&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-1"
+  },
+  {
+    id: 2,
+    name: "NYX PROFESSIONAL MAKEUP Mechanical Eye Liner Pencil, Black",
+    price: 5.50,
+    rating:3,
+    description: "A line and defined eye is just a twist away with this eyeliner pen. Intense pigment covers like a liquid eyeliner while its creamy formula holds for hours. No need to sharpen just twist-up and apply for a winged-out eyeliner look.",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/51B3tBa3eEL._SL1000_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/61%2BxWbrhAqL._SL1000_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/51-2QBdXFNL._SL1000_.jpg",
+    likes: 250,link:"https://www.amazon.com/NYX-Mechanical-Eye-Pencil-Black/dp/B005G9E9AW/ref=sr_1_4?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=CN80136S063SQVAKX3Q8&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-4"
+  },
+  {
+    id: 3,
+    name: "Coty Airspun Loose Face Powder, Translucent, Pack of 1",
+    price: 6.99,
+    rating:4,
+    description: "Coty Airspun face powder can be used to help smooth & soften the appearance of your skin as a foundation coverage, or use the loose powder on top of your makeup as a setting powder so you can get that flawless, smooth look",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/71le2ZYj97L._SL1250_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81Lj4B9Ob8L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/814XYPXz2ZL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Airspun-Translucent-Setting-Foundation-Lightweight/dp/B000BNG4VU/ref=sr_1_9?dchild=1&fst=as%3Aoff&pf_rd_i=16225006011&pf_rd_m=ATVPDKIKX0DER&pf_rd_p=fd971cf1-cdbc-4301-8c91-0a56b1c1e13f&pf_rd_r=CN80136S063SQVAKX3Q8&pf_rd_s=merchandised-search-4&pf_rd_t=101&qid=1488484834&rnid=16225006011&s=beauty-intl-ship&sr=1-9"
+  },
+  {
+    id: 4,
+    name: "Anastasia Beverly Hills - Eyeshadow Palette",
+    price: 45,
+    rating:5,
+    description: "Brand	Anastasia Beverly Hills,Item Form	Spray Color	Jackie Aina ,Finish Type	Metallic,matte ,Age Range (Description)	Adult",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/61SYAdMuu-L._SL1200_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/61pJtoMzmcL._SL1200_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/71YPlULP01L._SL1200_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B07X1W3MRP/ref=va_live_carousel?pf_rd_r=9W36SWJMYHP2R140H2T1&pf_rd_p=14936841-ca05-4835-b3bf-87617ecd98a6&pf_rd_m=ATVPDKIKX0DER&pf_rd_s=desktop-dp-sims&pd_rd_w=nKjZu&pd_rd_r=1268aac9-919a-47c3-991e-8fae8aa8ba36&pd_rd_wg=aEmWq&linkCode=ilv&tag=a03430e4-20&ascsubtag=Everyday_Makeup_Tutorial_and_Must_Have_Amazon_Finds_210223160038&pd_rd_i=B07X1W3MRP"
+  },
+  {
+    id: 5,
+    name: "TRESemmé Cleansing Shampoo",
+    price: 23.88,
+    rating:2,
+    description: "TRESemmé Cleansing Shampoo for Daily Use, Clean and Replenish Vitamin C and Green Tea Clarifying Shampoo Formula, 28 oz, Pack of 6",
+    image1:"https://m.media-amazon.com/images/I/81HIeD5V9qL._AC_SX410_SY308_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81RvgJ0pH0L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81BrzA-ahZL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Tresemme-Shampoo-Purify-Replenish-Cleansing/dp/B07FTGWYDY?ref_=ast_slp_dp"
+  }
+  ]
+  },
+  { category_id: 1,
+    category_name: "Cleaning",
+    display_flag: false, 
+    items: [{
+    id: 1,
+    name: "TRESemmé Cleansing Shampoo",
+    price: 23.88,
+    rating:2,
+    description: "TRESemmé Cleansing Shampoo for Daily Use, Clean and Replenish Vitamin C and Green Tea Clarifying Shampoo Formula, 28 oz, Pack of 6",
+    image1:"https://m.media-amazon.com/images/I/81HIeD5V9qL._AC_SX410_SY308_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81RvgJ0pH0L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81BrzA-ahZL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Tresemme-Shampoo-Purify-Replenish-Cleansing/dp/B07FTGWYDY?ref_=ast_slp_dp"
+  },
+  {
+    id: 2,
+    name: "EcoTools Makeup Brush Set for Eyeshadow, Foundation, Blush, and Concealer, Set of 5",
+    price: 6.99,
+    rating:1,
+    description: "Whether they're travel size makeup brushes, eye makeup brushes, foundation brushes or big brushes, the EcoTools makeup brushes flawlessly help apply products to your face.",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/61UsB%2BLieoL._SL1000_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/91uMrTT-Z3L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/71iYXWrA1WL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Ecotools-Makeup-Brush-Cleansing-Shampoo/dp/B01BSB56OY/ref=pd_vtp_2?pd_rd_w=HjbJI&pf_rd_p=18ed5dd9-54a4-4e64-9eb2-be78385d36ed&pf_rd_r=9ARY6RYRXVMFGFZ99FQS&pd_rd_r=d2cfeb24-9884-48b4-a3b5-866784e0f864&pd_rd_wg=KyJcn&pd_rd_i=B01BSB56OY&psc=1"
+  },
+  {
+    id: 3,
+    name: "Ecotools Makeup Brush Cleaner Cleansing Shampoo, 6 oz (Packaging May Vary)",
+    price: 6.99,
+    rating:4,
+    description: "Cleanses makeup brushes, applicators, and makeup sponges without leaving oily residue on the bristles.Safe for weekly use with a gentle massage or a brush cleaning mat.Dermatologist tested, hypoallergenic and free of parabens, phthalates, and petroleum-based ingredients",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/41%2BFIwSJNfL.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/51MLiaDVDyL._SL1383_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/612JwL1oo7L._SL1188_.jpg",
+    likes: 250,link:"https://www.amazon.com/Ecotools-Makeup-Brush-Cleansing-Shampoo/dp/B01BSB56OY/ref=pd_vtp_2?pd_rd_w=HjbJI&pf_rd_p=18ed5dd9-54a4-4e64-9eb2-be78385d36ed&pf_rd_r=9ARY6RYRXVMFGFZ99FQS&pd_rd_r=d2cfeb24-9884-48b4-a3b5-866784e0f864&pd_rd_wg=KyJcn&pd_rd_i=B01BSB56OY&psc=1"
+  },
+  {
+    id: 4,
+    name: "Luxe Makeup Brush Cleaner",
+    price: 29.99,
+    rating:5,
+    description: "Luxe Makeup Brush Cleaner - 5oz Makeup Cleaning Solution Included |USB Charging Station| 3 Adjustable Speeds| Instantly Wash and Dry Your Makeup Brushes",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/81R7YXukGhL._SL1500_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81jHzxYJUAL._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81bo4vCtTEL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B01MT6H91G/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B01MT6H91G&pd_rd_w=BYz8I&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=bDUGT&pf_rd_r=PYEF8E5QVQ12QGX697BW&pd_rd_r=2ae6695d-70f2-4d77-930e-dc0e486f922d&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSkNLMkRKR1A5VVMxJmVuY3J5cHRlZElkPUEwMzMyMTU2WkM3SzhOTUhWMzZJJmVuY3J5cHRlZEFkSWQ9QTAxMzY1OTMyTE1ENktPNlU4SlBZJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 5,
+    name: "BEAUTYBLENDER BLENDERCLEANSER",
+    price: 16,
+    rating:3,
+    description: "BEAUTYBLENDER BLENDERCLEANSER Lavender Solid for Cleaning Makeup Sponges, Brushes & Applicators, 1 oz. Vegan, Cruelty Free and Made in The USA",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/21GuJssMRBL._AC_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/11yHxMt3wpL._AC_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/61-F-IIzDNL._AC_SL1000_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B0085BVFZK/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B0085BVFZK&pd_rd_w=VsXWB&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=3NxEZ&pf_rd_r=M7WP0V902DTMNT8T6MP2&pd_rd_r=701018a7-f237-4cad-b329-ce133b6fbd8d&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQ0MyOVZKWVZBVEMyJmVuY3J5cHRlZElkPUEwMDUwNTkzMllYSUUyMDZQRkhXNiZlbmNyeXB0ZWRBZElkPUEwNTE4NzE4M1RHTTVBTDRSUzNMWCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+  }]
+  },
+    {category_id: 2,
+    category_name: "Cleaning",
+    display_flag: false, 
+    items: [
+  {
+    id: 1,
+    name: "BEAUTYBLENDER BLENDERCLEANSER",
+    price: 16,
+    rating:3,
+    description: "BEAUTYBLENDER BLENDERCLEANSER Lavender Solid for Cleaning Makeup Sponges, Brushes & Applicators, 1 oz. Vegan, Cruelty Free and Made in The USA",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/21GuJssMRBL._AC_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/11yHxMt3wpL._AC_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/61-F-IIzDNL._AC_SL1000_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B0085BVFZK/ref=sspa_dk_detail_5?psc=1&pd_rd_i=B0085BVFZK&pd_rd_w=VsXWB&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=3NxEZ&pf_rd_r=M7WP0V902DTMNT8T6MP2&pd_rd_r=701018a7-f237-4cad-b329-ce133b6fbd8d&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQ0MyOVZKWVZBVEMyJmVuY3J5cHRlZElkPUEwMDUwNTkzMllYSUUyMDZQRkhXNiZlbmNyeXB0ZWRBZElkPUEwNTE4NzE4M1RHTTVBTDRSUzNMWCZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+  },
+  {
+    id: 2,
+    name: "Makeup Brush Cleaner",
+    price: 19.98,
+    rating:4,
+    description: "Makeup Brush Cleaner – Rantizon Automatic Cosmetic Brushes Cleaning Machine with 360 Degree Rotation, Professional Makeup Brush Cleaning Tool, Cleans and Dries with 8 Rubbers",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/61ygreT3QZL._SL1200_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/61gPdCf328L._SL1000_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/61cgs9L0ujL._SL1000_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B08K8NLHFG/ref=sspa_dk_detail_2?psc=1&pd_rd_i=B08K8NLHFG&pd_rd_w=bNHzm&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a1&pd_rd_wg=iXiOF&pf_rd_r=738FEY6B6CA9J703RQGX&pd_rd_r=cd243785-1e7b-4794-a0e3-d52ee4d4acaf&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUExQ0lQTEtINUVKS0gyJmVuY3J5cHRlZElkPUEwNTEzODgwMlpZTkcxWDgzOFFVMSZlbmNyeXB0ZWRBZElkPUEwNDE1OTc1M0I5V0E3MFVXVTRJMyZ3aWRnZXROYW1lPXNwX2RldGFpbCZhY3Rpb249Y2xpY2tSZWRpcmVjdCZkb05vdExvZ0NsaWNrPXRydWU="
+  },{
+    id: 3,
+    name: "Ecotools Makeup Brush Cleaner Cleansing Shampoo, 6 oz (Packaging May Vary)",
+    price: 6.99,
+    rating:4,
+    description: "Cleanses makeup brushes, applicators, and makeup sponges without leaving oily residue on the bristles.Safe for weekly use with a gentle massage or a brush cleaning mat.Dermatologist tested, hypoallergenic and free of parabens, phthalates, and petroleum-based ingredients",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/41%2BFIwSJNfL.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/51MLiaDVDyL._SL1383_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/612JwL1oo7L._SL1188_.jpg",
+    likes: 250,link:"https://www.amazon.com/Ecotools-Makeup-Brush-Cleansing-Shampoo/dp/B01BSB56OY/ref=pd_vtp_2?pd_rd_w=HjbJI&pf_rd_p=18ed5dd9-54a4-4e64-9eb2-be78385d36ed&pf_rd_r=9ARY6RYRXVMFGFZ99FQS&pd_rd_r=d2cfeb24-9884-48b4-a3b5-866784e0f864&pd_rd_wg=KyJcn&pd_rd_i=B01BSB56OY&psc=1"
+  },
+  {
+    id: 4,
+    name: "Luxe Makeup Brush Cleaner",
+    price: 29.99,
+    rating:5,
+    description: "Luxe Makeup Brush Cleaner - 5oz Makeup Cleaning Solution Included |USB Charging Station| 3 Adjustable Speeds| Instantly Wash and Dry Your Makeup Brushes",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/81R7YXukGhL._SL1500_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81jHzxYJUAL._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81bo4vCtTEL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B01MT6H91G/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B01MT6H91G&pd_rd_w=BYz8I&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=bDUGT&pf_rd_r=PYEF8E5QVQ12QGX697BW&pd_rd_r=2ae6695d-70f2-4d77-930e-dc0e486f922d&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSkNLMkRKR1A5VVMxJmVuY3J5cHRlZElkPUEwMzMyMTU2WkM3SzhOTUhWMzZJJmVuY3J5cHRlZEFkSWQ9QTAxMzY1OTMyTE1ENktPNlU4SlBZJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+   {
+    id: 5,
+    name: "TRESemmé Cleansing Shampoo",
+    price: 23.88,
+    rating:2,
+    description: "TRESemmé Cleansing Shampoo for Daily Use, Clean and Replenish Vitamin C and Green Tea Clarifying Shampoo Formula, 28 oz, Pack of 6",
+    image1:"https://m.media-amazon.com/images/I/81HIeD5V9qL._AC_SX410_SY308_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81RvgJ0pH0L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81BrzA-ahZL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Tresemme-Shampoo-Purify-Replenish-Cleansing/dp/B07FTGWYDY?ref_=ast_slp_dp"
+  },
+  ]
+  },{category_id: 3,
+    category_name: "Makeup",
+    display_flag: false, 
+    items: [{
+    id: 1,
+    name: "TRESemmé Cleansing Shampoo",
+    price: 23.88,
+    rating:2,
+    description: "TRESemmé Cleansing Shampoo for Daily Use, Clean and Replenish Vitamin C and Green Tea Clarifying Shampoo Formula, 28 oz, Pack of 6",
+    image1:"https://m.media-amazon.com/images/I/81HIeD5V9qL._AC_SX410_SY308_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81RvgJ0pH0L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81BrzA-ahZL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Tresemme-Shampoo-Purify-Replenish-Cleansing/dp/B07FTGWYDY?ref_=ast_slp_dp"
+  },
+  {
+    id: 2,
+    name: "EcoTools Makeup Brush Set for Eyeshadow, Foundation, Blush, and Concealer, Set of 5",
+    price: 6.99,
+    rating:1,
+    description: "Whether they're travel size makeup brushes, eye makeup brushes, foundation brushes or big brushes, the EcoTools makeup brushes flawlessly help apply products to your face.",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/61UsB%2BLieoL._SL1000_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/91uMrTT-Z3L._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/71iYXWrA1WL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/Ecotools-Makeup-Brush-Cleansing-Shampoo/dp/B01BSB56OY/ref=pd_vtp_2?pd_rd_w=HjbJI&pf_rd_p=18ed5dd9-54a4-4e64-9eb2-be78385d36ed&pf_rd_r=9ARY6RYRXVMFGFZ99FQS&pd_rd_r=d2cfeb24-9884-48b4-a3b5-866784e0f864&pd_rd_wg=KyJcn&pd_rd_i=B01BSB56OY&psc=1"
+  },
+  {
+    id: 3,
+    name: "Ecotools Makeup Brush Cleaner Cleansing Shampoo, 6 oz (Packaging May Vary)",
+    price: 6.99,
+    rating:4,
+    description: "Cleanses makeup brushes, applicators, and makeup sponges without leaving oily residue on the bristles.Safe for weekly use with a gentle massage or a brush cleaning mat.Dermatologist tested, hypoallergenic and free of parabens, phthalates, and petroleum-based ingredients",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/41%2BFIwSJNfL.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/51MLiaDVDyL._SL1383_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/612JwL1oo7L._SL1188_.jpg",
+    likes: 250,link:"https://www.amazon.com/Ecotools-Makeup-Brush-Cleansing-Shampoo/dp/B01BSB56OY/ref=pd_vtp_2?pd_rd_w=HjbJI&pf_rd_p=18ed5dd9-54a4-4e64-9eb2-be78385d36ed&pf_rd_r=9ARY6RYRXVMFGFZ99FQS&pd_rd_r=d2cfeb24-9884-48b4-a3b5-866784e0f864&pd_rd_wg=KyJcn&pd_rd_i=B01BSB56OY&psc=1"
+  },
+  {
+    id: 4,
+    name: "Luxe Makeup Brush Cleaner",
+    price: 29.99,
+    rating:5,
+    description: "Luxe Makeup Brush Cleaner - 5oz Makeup Cleaning Solution Included |USB Charging Station| 3 Adjustable Speeds| Instantly Wash and Dry Your Makeup Brushes",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/81R7YXukGhL._SL1500_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/81jHzxYJUAL._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/81bo4vCtTEL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B01MT6H91G/ref=sspa_dk_detail_1?psc=1&pd_rd_i=B01MT6H91G&pd_rd_w=BYz8I&pf_rd_p=cbc856ed-1371-4f23-b89d-d3fb30edf66d&pd_rd_wg=bDUGT&pf_rd_r=PYEF8E5QVQ12QGX697BW&pd_rd_r=2ae6695d-70f2-4d77-930e-dc0e486f922d&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUEzSkNLMkRKR1A5VVMxJmVuY3J5cHRlZElkPUEwMzMyMTU2WkM3SzhOTUhWMzZJJmVuY3J5cHRlZEFkSWQ9QTAxMzY1OTMyTE1ENktPNlU4SlBZJndpZGdldE5hbWU9c3BfZGV0YWlsX3RoZW1hdGljJmFjdGlvbj1jbGlja1JlZGlyZWN0JmRvTm90TG9nQ2xpY2s9dHJ1ZQ=="
+  },
+  {
+    id: 5,
+    name: "NewBang 5 Colors Waterproof Color Mascara",
+    price: 9.99,
+    rating:5,
+    description: "NewBang 5 Colors Waterproof Color Mascara,  Long Lasting Color Mascara,Natural formula（Yellow, orange, red, green, blue）",
+    image1:"https://images-na.ssl-images-amazon.com/images/I/712kk3q1R1L._SL1500_.jpg",
+    image2:"https://images-na.ssl-images-amazon.com/images/I/71lLNFckfqL._SL1500_.jpg",
+    image3:"https://images-na.ssl-images-amazon.com/images/I/71vIWSFTPfL._SL1500_.jpg",
+    likes: 250,link:"https://www.amazon.com/dp/B08P7H6PQX/ref=sspa_dk_detail_0?psc=1&pd_rd_i=B08P7H6PQX&pd_rd_w=SWg02&pf_rd_p=4269e1a0-a218-4fbd-9748-1cd337d2f2a5&pd_rd_wg=UeWwn&pf_rd_r=ZR22TWT8AFSY4WF6CYBX&pd_rd_r=623a9007-3827-4cae-b6c8-9b734aa729c3&spLa=ZW5jcnlwdGVkUXVhbGlmaWVyPUFJOFlYODRVUU1GUzYmZW5jcnlwdGVkSWQ9QTA2NDMwNDAzT0taWlRYVFJDVk1MJmVuY3J5cHRlZEFkSWQ9QTAyMTkxODE5Nk00OTlLMEJMN0Ymd2lkZ2V0TmFtZT1zcF9kZXRhaWwmYWN0aW9uPWNsaWNrUmVkaXJlY3QmZG9Ob3RMb2dDbGljaz10cnVl"
+  }
+]}
+];
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
